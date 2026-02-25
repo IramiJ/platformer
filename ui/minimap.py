8d94f263ab3cf754be997656e51ebca0721b70dc
IramiJ <yugiohduel@gmail.com>
2026-02-25 22:30:59 +0100

    worked on the minimap

--- ui/minimap.py
+++ ui/minimap.py
@@ -1,6 +1,8 @@
 class Minimap:
     def __init__(self):
-        pass
+        self.size = [40, 30]
+        self.pos = [280, 0]
+        self.map_array = []
     def load_map(self, map):
         pass
     def render(self, display):
