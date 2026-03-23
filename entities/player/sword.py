a1bafa2f19b0958a45df99b4dd6d4ed3fdc76121
Colliname <yugiohduel3@gmail.com>
2026-03-23 21:08:13 +0100

    refactored the sword class into smaller functions

--- entities/player/sword.py
+++ entities/player/sword.py
@@ -16,19 +16,26 @@ class Sword(entity):
         self.animation_database["idle"] = [self.img]
     def add_particles(self):
         if self.particle_cd == 0:
-            for i in range(0, 4):
-                p = Particle("assets/particles/sword_particle.png", [self.loc[0]+i, self.loc[1]], 120)
-                p.y_velocity += i/100                
-                if self.flip:                   
-                    self.particles.append(p)                    
-                else:
-                    p.loc[0] = self.loc[0]+20-i
-                    self.particles.append(p)
-            self.particle_cd = 60
+            self.spawn_particles()
         else:
             self.particle_cd -= 1
+
+        self.decrease_particles_duration()
+    def spawn_particles(self):
+        for i in range(0, 4):
+            p = Particle("assets/particles/sword_particle.png", [self.loc[0]+i, self.loc[1]], 120)
+            p.y_velocity += i/100                
+            if self.flip:                   
+                self.particles.append(p)                    
+            else:
+                p.loc[0] = self.loc[0]+20-i
+                self.particles.append(p)
+        self.particle_cd = 60
+
+    def decrease_particles_duration(self):
         for particle in self.particles:
             particle.duration -= 1
+
     def load_slice(self):
         path = "assets/weapons/sword/slice"
         dur = [1 for x in range(11)]
@@ -43,27 +50,39 @@ class Sword(entity):
             for i in range(frame):
                 self.slice_animation.append(animation_image)
             n +=1 
+
     def draw_slice(self, display: pygame.Surface, scroll, dt):
         if not self.flip:
             display.blit(pygame.transform.flip(self.slice_animation[math.floor(self.slice_frame)],self.flip,False), [self.loc[0]+10-scroll.render_scroll[0], self.loc[1]-scroll.render_scroll[1]-16])
         else:
             display.blit(pygame.transform.flip(self.slice_animation[math.floor(self.slice_frame)],self.flip,False), [self.loc[0]-self.slice_animation[math.floor(self.slice_frame)].get_width()+10-scroll.render_scroll[0], self.loc[1]-scroll.render_scroll[1]-16])
+        self.update_frame(dt)
+
+    def update_frame(self, dt):
         self.slice_frame += dt    
         if self.slice_frame >= len(self.slice_animation):
             self.slice_frame = 0
 
     def draw(self, player, display, scroll, dt):
-        self.flip = player.flip     
+        self.set_flip(player)
+        self.update_location(player)
+
+        if player.dashing:
+            self.draw_slice(display, scroll, dt)
+
+        self.add_particles()
+        display.blit(pygame.transform.flip(self.img, self.flip, False), [self.loc[0] - scroll.render_scroll[0], self.loc[1] - scroll.render_scroll[1]])
+        self.draw_particles(display, scroll)
+        
+    def set_flip(self, player):
+        self.flip = player.flip
+    
+    def update_location(self, player):
         if not player.flip:
             self.loc = [player.rect.right-6, player.rect.y+6]
         else:
-            
             self.loc = [player.rect.left-self.img.get_width()+6, player.rect.y+6]
-        if player.dashing:
-            self.draw_slice(display, scroll, dt)
-        self.add_particles()
-        display.blit(pygame.transform.flip(self.img, self.flip, False), [self.loc[0] - scroll.render_scroll[0], self.loc[1] - scroll.render_scroll[1]])
+    
+    def draw_particles(self, display, scroll):
         for particle in self.particles:
             particle.render(display, scroll)
-        
-
