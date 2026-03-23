7fa0d1d37df07689fde824aad80c3d8589b66036
Colliname <yugiohduel3@gmail.com>
2026-03-23 21:15:08 +0100

    refactored the render and buy

--- ui/shopping.py
+++ ui/shopping.py
@@ -21,28 +21,37 @@ class Shop():
             self.imgs[entry] = pygame.image.load(self.data[entry]["asset_path"]).convert(), [0, 32 * counter]
             self.item_boxes[entry] = pygame.Rect(self.imgs[entry][1][0],self.imgs[entry][1][1],self.imgs[entry][0].get_width(),self.imgs[entry][0].get_height())
             counter += 1
+
     def render(self,surf, player_coin_amount):
         surf.fill((0,0,0))
         self.large_font.render(surf,'SHOP',(150,0))  
         draw_constants(surf)
+        self.draw_items(surf)
+        self.large_font.render(surf,str(player_coin_amount), (16,0))
+
+    def draw_items(self, surf):
         for item in self.imgs:
             self.small_font.render(surf,item,(0,self.imgs[item][1][1]-8))
             self.large_font.render(surf,self.prices[item],[36,self.imgs[item][1][1]+2])
             self.small_font.render(surf, "duration: " + str(self.data[item]["duration"]), [60,self.imgs[item][1][1]+2]), 
             surf.blit(self.imgs[item][0], (self.item_boxes[item].x,self.item_boxes[item].y))
-        self.large_font.render(surf,str(player_coin_amount), (16,0))
+            
     def buy(self, player, buff_list):
         for item in self.imgs:
             if pygame.mouse.get_pressed()[0]:
                 mouse_rect = pygame.Rect(pygame.mouse.get_pos()[0]/2, pygame.mouse.get_pos()[1]/2,1,1)
-                if mouse_rect.colliderect(self.item_boxes[item]):
-                    if player.coin_amount >= int(self.prices[item]) and self.buy_cooldown == 0 and item not in buff_list:
-                        player.coin_amount -= int(self.prices[item])
-                        self.buy_cooldown = 0
-                        buff_list[item] = int(self.data[item]["duration"]) * 60
+                self.buy_on_press(mouse_rect, item, player, buff_list)
+    def buy_on_press(self, mouse_rect, item, player, buff_list):
+        if mouse_rect.colliderect(self.item_boxes[item]):
+            if player.coin_amount >= int(self.prices[item]) and self.buy_cooldown == 0 and item not in buff_list:
+                player.coin_amount -= int(self.prices[item])
+                self.buy_cooldown = 0
+                buff_list[item] = int(self.data[item]["duration"]) * 60            
+
     def change_displaying(self):
         self.displaying = not self.displaying
         return self.displaying
+    
     def show(self, display, player):
         if self.displaying:
             self.render(display, player.coin_amount)
