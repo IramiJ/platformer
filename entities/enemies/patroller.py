c2b2336de2c5d651ae08b3f550c5d125fd9c5359
IramiJ <yugiohduel3@gmail.com>
2026-03-23 16:05:54 +0100

    refactored the patroller class

--- entities/enemies/patroller.py
+++ entities/enemies/patroller.py
@@ -1,6 +1,5 @@
 from entities.enemies.enemy import Enemy
 from entities.animations import load_animation
-from entities.entity import simple_entity
 from entities.hp_bar import Hp_bar
 
 class Patroller(Enemy):
@@ -26,9 +25,11 @@ class Patroller(Enemy):
         if not self.alive:
             return 
         self.draw(display, scroll)
+        self.update_hp_bar_location(scroll)
+        self.hp_bar.draw(display, self.max_hp, self.hp)
+    def update_hp_bar_location(self, scroll):
         self.hp_bar.x = self.rect.x-scroll[0]
         self.hp_bar.y = self.rect.y-scroll[1]-20
-        self.hp_bar.draw(display, self.max_hp, self.hp)
     def move(self, dt):
         if self.stunned:
             self.stun_cd -= 1
@@ -36,25 +37,28 @@ class Patroller(Enemy):
                 self.stunned = False
         else:
             self.movement = [0, 0]
-            self.burst()
+            self.handle_burst()
             if self.direction == 'r':
-                if self.rect.x >= self.spawn_point[0] + self.distance:
-                    self.direction = 'l'
-                    self.flip = True
-                    self.activate_burst()
-                else:
-                    self.movement[0] += self.true_velocity
+                self.move_right()
             if self.direction == 'l':
-                if self.rect.x <= self.spawn_point[0] - self.distance:
-                    self.direction = 'r'
-                    self.flip = False
-                    self.activate_burst()
-                else:
-                    self.movement[0] -= self.true_velocity
+                self.move_left()
             self.rect.x += self.movement[0] * dt
+    def move_right(self):
+        if self.rect.x >= self.spawn_point[0] + self.distance:
+            self.direction = 'l'
+            self.flip = True
+            self.activate_burst()
+        else:
+            self.movement[0] += self.true_velocity
+    def move_left(self):
+        if self.rect.x <= self.spawn_point[0] - self.distance:
+            self.direction = 'r'
+            self.flip = False
+            self.activate_burst()
+        else:
+            self.movement[0] -= self.true_velocity
     def attack(self, player, scroll):
         if self.attack_cd > 0:
-            
             self.attack_cd -= 1
         else:
             if self.rect.colliderect(player.rect) and not player.dashing:
@@ -66,7 +70,7 @@ class Patroller(Enemy):
     def activate_burst(self):
         self.bursting = True
         self.burst_cd = 30
-    def burst(self):
+    def handle_burst(self):
         if self.bursting:
             self.true_velocity = self.velocity + self.burst_velocity
             self.burst_cd -= 1
