1b18646c3e85a1c337ef398464616e8b8cbbe706
Colliname <yugiohduel3@gmail.com>
2026-07-18 22:44:47 +0200

    added a coordinate determination file

---
+++ world/coordinates.py
@@ -0,0 +1,11 @@
+from core.settings import TILE_SIZE
+
+def tile_to_pixel(value: int) -> int:
+    return value * TILE_SIZE
+
+def pixel_to_tile(value: int | float) -> int:
+    return int(value // TILE_SIZE)
+
+def tile_position_to_pixel(position: list[int]) -> list[int]:
+    x, y = position
+    return tile_position_to_pixel(x), tile_position_to_pixel(y)
